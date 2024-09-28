2121cde384f6c6944106199db0c2795ec6021ab4
allu0786ansari <allu456654ansari@gmail.com>
2024-09-28 21:28:04 +0530

    setting updated

--- Backend/LMS/settings.py
+++ Backend/LMS/settings.py
@@ -123,9 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_URL = BASE_DIR / 'templates'
+STATIC_URL = '/static/'
+STATICFILEES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = BASE_DIR / 'templates'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
