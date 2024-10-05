cddb6f548d8e9c3dd55fa3e939680d90e2490483
allu0786ansari <allu456654ansari@gmail.com>
2024-10-05 12:06:48 +0530

    zustand added to keep tract of user information

---
+++ Frontend/store/auth.js
@@ -0,0 +1,31 @@
+import { create} from "zustand"
+import {mountStoreDevtools} from "simple-zustand-devtools"
+
+const useAuthStore = create((set, get) => ({
+    allUserData: null,
+    loading: false,
+
+    user: () => ({
+        user_id: get().allUserData?.user_id || null,
+        username: get().allUserData?.username || null,
+
+    }),
+
+    setUser: (user) => 
+        set({
+            allUserData: user,
+            
+        }),
+
+    setLoading: (loading) => set({loading}),
+    isLoggedIn: () => get().allUserData !== null,
+
+}));
+
+
+if (import.meta.env.DEV){
+    mountStoreDevtools("Store",useAuthStore);
+
+}
+
+export { useAuthStore };
