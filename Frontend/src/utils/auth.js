6482bd290f6ffcbc4cd9526ed8a8ad18e86151d3
Allaudin Ansari <allu456654ansari@gmail.com>
2024-11-02 14:44:31 +0530

    setuser is added in auth.js

--- Frontend/src/utils/auth.js
+++ Frontend/src/utils/auth.js
@@ -24,3 +24,48 @@ export const login = async (email, password) => {
         }
     }
 };
+
+export const register = async (full_name, email, password,  password2) => {
+    try{
+        const {data} = await axios.post(`user/register/`, {
+            full_name,
+            email,
+            password,
+            password2
+        });
+
+        await login(email, password);
+        alert("Registeration Successful");
+        return { data, error: null};
+    } catch(error) {
+        return {
+            data: null,
+            error: error.response.data?.detail || "Something went wrong",
+        }
+    }
+
+};
+
+export const logout = () => {
+    Cookie.remove("access_token");
+    Cookie.remove("refresh_token");
+    useAuthStore.getState().setUser(null);
+    alert("You have been logged out");
+};
+
+export const setUser = async () => {
+    const access_token = Cookie.get("access_token");
+    const refresh_token = Cookie.get("refresh_token");
+
+    if ( !access_token || !refresh_token ) {
+        alert("Toekn does not exists");
+        return;
+    }
+    if ( isAccessTokenExpired(access_token)) {
+        const response =getRefreshedToken(refresh_token);
+        setAuthUser(response.access, response.refresh);
+    } else {
+        setAuthUser(access_token, refresh_token);
+    }
+
+};
