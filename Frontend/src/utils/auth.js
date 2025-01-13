b511d7d88c03062f88ed8a475c92eb04303206d6
Allaudin Ansari <allu456654ansari@gmail.com>
2025-01-13 22:41:50 +0530

    register updated

--- Frontend/src/utils/auth.js
+++ Frontend/src/utils/auth.js
@@ -1,107 +1,109 @@
-import {useAuthStore} from "../store/auth";
+import { useAuthStore } from "../store/auth";
 import axios from "./axios";
 import jwt_decode from "jwt-decode";
 import Cookie from "js-cookie";
 import Swal from "sweetalert2";
 
 export const login = async (email, password) => {
-    try{
-        const { data, status } = await axios.post(`user/token/`, {
-            email,
-            password
-        });
-        
-        if(status === 200){
-            setAuthUser(data.accesss, data.refresh);
-            alert("Login Successful");
-        }
-        return {data, error: null};
+  try {
+    const { data, status } = await axios.post(`user/token/`, {
+      email,
+      password,
+    });
 
-    } catch (error){
-        return {
-            data: null,
-            error: error.response.data?.detail || "Something went wrong",
-        }
-    }
-};
-
-export const register = async (full_name, email, password,  password2) => {
-    try{
-        const {data} = await axios.post(`user/register/`, {
-            full_name,
-            email,
-            password,
-            password2
-        });
-
-        await login(email, password);
-        alert("Registeration Successful");
-        return { data, error: null};
-    } catch(error) {
-        return {
-            data: null,
-            error: error.response.data?.detail || "Something went wrong",
-        }
+    if (status === 200) {
+      setAuthUser(data.access, data.refresh);
     }
 
+    return { data, error: null };
+  } catch (error) {
+    return {
+      data: null,
+      error: error.response.data?.detail || "Something went wrong",
+    };
+  }
 };
+
+export const register = async (full_name, email, password, password2) => {
+  try {
+    const { data } = await axios.post(`user/register/`, {
+      full_name,
+      email,
+      password,
+      password2,
+    });
+
+    await login(email, password);
+    return { data, error: null };
+  } catch (error) {
+    const fullNameError = error.response?.data?.full_name || "Full name error not provided";
+    const emailError = error.response?.data?.email || "Email error not provided";
+    const generalError = error.response?.data?.detail || "An unexpected error occurred";
+
+    return {
+      data: null,
+      error: `${fullNameError} - ${emailError} || ${generalError}`,
+    };
+  }
+};
+
 
 export const logout = () => {
-    Cookie.remove("access_token");
-    Cookie.remove("refresh_token");
-    useAuthStore.getState().setUser(null);
-    alert("You have been logged out");
+  Cookie.remove("access_token");
+  Cookie.remove("refresh_token");
+  useAuthStore.getState().setUser(null);
 };
 
 export const setUser = async () => {
-    const access_token = Cookie.get("access_token");
-    const refresh_token = Cookie.get("refresh_token");
+  const access_token = Cookie.get("access_token");
+  const refresh_token = Cookie.get("refresh_token");
 
-    if ( !access_token || !refresh_token ) {
-      //  alert("Toekn does not exists");
-        return;
-    }
-    if ( isAccessTokenExpired(access_token)) {
-        const response =getRefreshedToken(refresh_token);
-        setAuthUser(response.access, response.refresh);
-    } else {
-        setAuthUser(access_token, refresh_token);
-    }
+  if (!access_token || !refresh_token) {
+    // alert("Tokens does not exists");
+    return;
+  }
 
+  if (isAccessTokenExpired(access_token)) {
+    const response = getRefreshedToken(refresh_token);
+    setAuthUser(response.access, response.refresh);
+  } else {
+    setAuthUser(access_token, refresh_token);
+  }
 };
 
 export const setAuthUser = (access_token, refresh_token) => {
-    Cookie.set("access_token", access_token, {
-        expires: 1,
-        secure: true,
-    });
+  Cookie.set("access_token", access_token, {
+    expires: 1,
+    secure: true,
+  });
 
-    Cookie.set("refresh_token", refresh_token, {
-        expires: 7,
-        secure: true,
-    });
+  Cookie.set("refresh_token", refresh_token, {
+    expires: 7,
+    secure: true,
+  });
 
-    const user = jwt_decode(access_token) ?? null;
-    if (user) {
-        userAuthStore.getState().setUser(user);
-    }
-    useAuthStore.getState().setLoading(false);
+  const user = jwt_decode(access_token) ?? null;
+
+  if (user) {
+    useAuthStore.getState().setUser(user);
+  }
+  useAuthStore.getState().setLoading(false);
 };
 
 export const getRefreshedToken = async () => {
-    const refresh_token = Cookie.get("refresh_token");
-    const response = await axios.post(`token/refresh/`, {
-        refresh: refresh_token,
-    });
-    return response.data;
+  const refresh_token = Cookie.get("refresh_token");
+  const response = await axios.post(`user/token/refresh/`, {
+    refresh: refresh_token,
+  });
+  return response.data;
 };
 
 export const isAccessTokenExpired = (access_token) => {
-    try {
-        const decodedToken = jwt_decode(access_token);
-        return decodedToken.exp < Date.now() / 1000;
-    } catch (error) {
-        console.log(error);
-        return true;
-    }
+  try {
+    const decodedToken = jwt_decode(access_token);
+    return decodedToken.exp < Date.now() / 1000;
+  } catch (error) {
+    console.log(error);
+    return true;
+  }
 };
--- Frontend/src/views/auth/Register.jsx
+++ Frontend/src/views/auth/Register.jsx
@@ -1,32 +1,44 @@
 import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 import apiInstance from "../../utils/axios";
 import { register } from "../../utils/auth";
 
-import React from 'react'
-import BaseHeader from '../partials/BaseHeader'
-import BaseFooter from '../partials/BaseFooter'
-import { Link } from 'react-router-dom'
-
+import BaseHeader from "../partials/BaseHeader";
+import BaseFooter from "../partials/BaseFooter";
 
 function Register() {
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [password2, setPassword2] = useState("");
-  const [isLoading, setisLoading ] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
-    setisLoading(true)
-    await register(fullName, email, password, password2);
+    e.preventDefault();
+    setIsLoading(true);
+
+    const { error } = await register(fullName, email, password, password2);
+    if (error) {
+      alert(error);
+      setIsLoading(false);
+    } else {
+      navigate("/");
+      alert("Registration Successfull, you have now been logged in");
+      setIsLoading(false);
+    }
   };
 
   return (
     <>
       <BaseHeader />
 
-      <section className="container d-flex flex-column vh-100" style={{ marginTop: "150px" }}>
+      <section
+        className="container d-flex flex-column vh-100"
+        style={{ marginTop: "150px" }}
+      >
         <div className="row align-items-center justify-content-center g-0 h-lg-100 py-8">
           <div className="col-lg-5 col-md-8 py-8 py-xl-0">
             <div className="card shadow">
@@ -41,10 +53,16 @@ function Register() {
                   </span>
                 </div>
                 {/* Form */}
-                <form className="needs-validation" noValidate="" onSubmit={handleSubmit} >
+                <form
+                  className="needs-validation"
+                  noValidate=""
+                  onSubmit={handleSubmit}
+                >
                   {/* Username */}
                   <div className="mb-3">
-                    <label htmlFor="email" className="form-label">Full Name</label>
+                    <label htmlFor="full_name" className="form-label">
+                      Full Name
+                    </label>
                     <input
                       type="text"
                       id="full_name"
@@ -56,7 +74,9 @@ function Register() {
                     />
                   </div>
                   <div className="mb-3">
-                    <label htmlFor="email" className="form-label">Email Address</label>
+                    <label htmlFor="email" className="form-label">
+                      Email Address
+                    </label>
                     <input
                       type="email"
                       id="email"
@@ -67,10 +87,12 @@ function Register() {
                       onChange={(e) => setEmail(e.target.value)}
                     />
                   </div>
-                  
+
                   {/* Password */}
                   <div className="mb-3">
-                    <label htmlFor="password" className="form-label">Password</label>
+                    <label htmlFor="password" className="form-label">
+                      Password
+                    </label>
                     <input
                       type="password"
                       id="password"
@@ -82,10 +104,12 @@ function Register() {
                     />
                   </div>
                   <div className="mb-3">
-                    <label htmlFor="password" className="form-label">Confirm Password</label>
+                    <label htmlFor="confirm_password" className="form-label">
+                      Confirm Password
+                    </label>
                     <input
                       type="password"
-                      id="password"
+                      id="password2"
                       className="form-control"
                       name="password"
                       placeholder="**************"
@@ -95,9 +119,21 @@ function Register() {
                   </div>
                   <div>
                     <div className="d-grid">
-                      <button type="submit" className="btn btn-primary">
-                        Sign Up <i className='fas fa-user-plus'></i>
-                      </button>
+                      {isLoading === true && (
+                        <button
+                          disabled
+                          type="submit"
+                          className="btn btn-primary"
+                        >
+                          Processing <i className="fas fa-spinner fa-spin"></i>
+                        </button>
+                      )}
+
+                      {isLoading === false && (
+                        <button type="submit" className="btn btn-primary">
+                          Sign Up <i className="fas fa-user-plus"></i>
+                        </button>
+                      )}
                     </div>
                   </div>
                 </form>
@@ -109,7 +145,7 @@ function Register() {
 
       <BaseFooter />
     </>
-  )
+  );
 }
 
-export default Register
+export default Register;
